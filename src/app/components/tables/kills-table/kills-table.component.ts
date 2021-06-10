2a5a5c1af933a8928cec4b8a1ad4faeb67b5b646
patricknnn <patrickniewold@gmail.com>
2021-06-10 08:36:57 +0200

    updated kills table

--- src/app/components/tables/kills-table/kills-table.component.ts
+++ src/app/components/tables/kills-table/kills-table.component.ts
@@ -1,3 +1,4 @@
+import { CssSelector } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { LifetimeStats } from 'src/app/models/lifetime-stats';
 import { DynamicTableColumnConfig } from 'src/app/modules/dynamic-tables/models/dynamic-table-column-config';
@@ -13,8 +14,11 @@ import { TableService } from 'src/app/services/table.service';
 })
 export class KillsTableComponent implements OnInit {
   tableConfig?: DynamicTableConfig;
-  columnConfig?: DynamicTableColumnConfig[];
-  data?: any;
+  columnConfig: DynamicTableColumnConfig[] = [];
+  data: { name: string, data: any }[] = [];
+  marksmanData: { name: string, total: number, kar: number, spr: number, mk2: number, sks: number, cross: number, mbr: number }[] = [];
+  sniperdata: { name: string, total: number, ax50: number, delta: number, hdr: number, xm: number }[] = [];
+  assaultData: { name: string, total: number, ak: number, an: number, as12: number, fal: number, fala: number, gal: number, kilo: number, mc: number, m4: number, sc: number, s5: number, m21: number, valp: number }[] = [];
 
   constructor(
     private tables: TableService,
@@ -68,18 +72,19 @@ export class KillsTableComponent implements OnInit {
         draggable: true
       }),
       new DynamicTableColumnConfig({
-        key: 'garand',
+        key: 'mbr',
         header: 'Garand 2.0',
         sortable: true,
         draggable: true
       }),
     ];
-    this.getMarksmanKillData().then((res: any) => {
+    this.getData().then((res) => {
       this.data = res;
+      this.getMarksmanKillData();
     });
   }
 
-  getMarksmanKillData(): Promise<any[]> {
+  getData(): Promise<{ name: string, data: any }[]> {
     return new Promise((resolve, reject) => {
       let data: any[] = [];
       let count = 0;
@@ -92,25 +97,122 @@ export class KillsTableComponent implements OnInit {
               reject("An error occured");
             }
             let name = player.name;
-            let total = res.lifetime.all.properties.kills;
-            let kar = res.lifetime.itemData.weapon_marksman.iw8_sn_kilo98.properties.kills;
-            let spr = res.lifetime.itemData.weapon_marksman.iw8_sn_romeo700.properties.kills;
-            let mk2 = res.lifetime.itemData.weapon_marksman.iw8_sn_sbeta.properties.kills;
-            let sks = res.lifetime.itemData.weapon_marksman.iw8_sn_sksierra.properties.kills;
-            let cross = res.lifetime.itemData.weapon_marksman.iw8_sn_crossbow.properties.kills;
-            let garand = res.lifetime.itemData.weapon_marksman.iw8_sn_mike14.properties.kills;
-            data.push({ name: name, total: total, kar: kar, spr: spr, mk2: mk2, sks: sks, cross: cross, grand: garand });
+            let lifetime = res.lifetime;
+            // weapons
+            let smg = res.lifetime.itemData.weapon_smg;
+            let shotty = res.lifetime.itemData.weapon_shotgun;
+            let melee = res.lifetime.itemData.weapon_melee;
+            let launcher = res.lifetime.itemData.weapon_launcher;
+            let other = res.lifetime.itemData.weapon_other;
+            let pistol = res.lifetime.itemData.weapon_pistol
+            let lmg = res.lifetime.itemData.weapon_lmg;
+            data.push({ name: name, data: lifetime });
             count++;
             if (count == players.length) {
               resolve(data);
             }
           })
           .catch((error) => {
-            reject("An error occured");
+            reject(error);
           });
       });
     });
   }
+
+  getMarksmanKillData(): void {
+    // empty
+    this.columnConfig = [];
+    this.marksmanData = [];
+    // data
+    this.data.forEach((entry) => {
+      let name = entry.name;
+      let total = entry.data.all.properties.kills;
+      let kar = entry.data.itemData.weapon_marksman.iw8_sn_kilo98.properties.kills;
+      let spr = entry.data.itemData.weapon_marksman.iw8_sn_romeo700.properties.kills;
+      let mk2 = entry.data.itemData.weapon_marksman.iw8_sn_sbeta.properties.kills;
+      let sks = entry.data.itemData.weapon_marksman.iw8_sn_sksierra.properties.kills;
+      let cross = entry.data.itemData.weapon_marksman.iw8_sn_crossbow.properties.kills;
+      let mbr = entry.data.itemData.weapon_marksman.iw8_sn_mike14.properties.kills;
+      this.marksmanData.push({ name: name, total: total, kar: kar, spr: spr, mk2: mk2, sks: sks, cross: cross, mbr: mbr });
+    });
+    // columns
+    for (const property in this.marksmanData[0]) {
+      this.columnConfig.push(
+        new DynamicTableColumnConfig({
+          key: property,
+          header: property,
+          sortable: true,
+          draggable: true
+        }),
+      );
+    }
+
+  }
+
+  getSniperKillData(): void {
+    // empty
+    this.columnConfig = [];
+    this.sniperdata = [];
+    // data
+    this.data.forEach((entry) => {
+      let name = entry.name;
+      let total = entry.data.all.properties.kills;
+      let ax50 = entry.data.itemData.weapon_sniper.iw8_sn_alpha50;
+      let delta = entry.data.itemData.weapon_sniper.iw8_sn_delta;
+      let hdr = entry.data.itemData.weapon_sniper.iw8_sn_hdromeo;
+      let xm = entry.data.itemData.weapon_sniper.iw8_sn_xmike109;
+      this.sniperdata.push({ name: name, total: total, ax50: ax50, delta: delta, hdr: hdr, xm: xm });
+    });
+    // columns
+    for (const property in this.sniperdata[0]) {
+      this.columnConfig.push(
+        new DynamicTableColumnConfig({
+          key: property,
+          header: property,
+          sortable: true,
+          draggable: true
+        }),
+      );
+    }
+  }
+
+  getAssaultKillData(): void {
+    // empty
+    this.columnConfig = [];
+    this.assaultData = [];
+    // data
+    this.data.forEach((entry) => {
+      let name = entry.name;
+      let total = entry.data.all.properties.kills;
+      let ak = entry.data.itemData.weapon_assault_rifle.iw8_ar_akilo47;
+      let an = entry.data.itemData.weapon_assault_rifle.iw8_ar_anovember94;
+      let as12 = entry.data.itemData.weapon_assault_rifle.iw8_ar_asierra12;
+      let fal = entry.data.itemData.weapon_assault_rifle.iw8_ar_falima;
+      let fala = entry.data.itemData.weapon_assault_rifle.iw8_ar_falpha;
+      let gal = entry.data.itemData.weapon_assault_rifle.iw8_ar_galima;
+      let kilo = entry.data.itemData.weapon_assault_rifle.iw8_ar_kilo433;
+      let mc = entry.data.itemData.weapon_assault_rifle.iw8_ar_mcharlie;
+      let m4 = entry.data.itemData.weapon_assault_rifle.iw8_ar_mike4;
+      let sc = entry.data.itemData.weapon_assault_rifle.iw8_ar_scharlie;
+      let s5 = entry.data.itemData.weapon_assault_rifle.iw8_ar_sierra552;
+      let m21 = entry.data.itemData.weapon_assault_rifle.iw8_ar_tango21;
+      let valp = entry.data.itemData.weapon_assault_rifle.iw8_ar_valpha;
+      this.assaultData.push({ name: name, total: total, ak: ak, an: an, as12: as12, fal: fal, fala: fala, gal: gal, kilo: kilo, mc: mc, m4: m4, sc: sc, s5: s5, m21: m21, valp: valp });
+    });
+    // columns
+    for (const property in this.assaultData[0]) {
+      this.columnConfig.push(
+        new DynamicTableColumnConfig({
+          key: property,
+          header: property,
+          sortable: true,
+          draggable: true
+        }),
+      );
+    }
+  }
+
+
 
   handleSelectionChangeEvent(event: any): void {
     console.log(event);
